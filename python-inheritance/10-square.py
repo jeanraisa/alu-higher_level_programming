5b954dc0bd1911ca9e08c55a73373a41cb1a1665
root <r.irakoze@alustudent.com>
2023-03-23 06:01:07 -0700

    Square inherts from Rectangle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
-""" Base Geometry Class """
+""" Class inherited from rectangle """
 
 
-class BaseGeometry:
-    """ class that improve geometry with integer validator"""
-    def area(self):
-        """ raises an Exception with the message area() is not implemented """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ validates value """
-        if (type(value) is not int):
-            raise TypeError(name + " must be an integer")
-        if (value <= 0):
-            raise ValueError(name + " must be greater than 0")
-
-""" Program that build a full rectangle """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(BaseGeometry):
-    """ class Rectangle that inherits from BaseGeometry """
-    def __init__(self, width, height):
-        """ Constructor """
-        self.__width = width
-        self.__height = height
-        BaseGeometry.integer_validator(self, "width", self.__width)
-        BaseGeometry.integer_validator(self, "height", self.__height)
+class Square(Rectangle):
+    """ class Square that inherits from Rectangle (9-rectangle.py) """
+    def __init__(self, size):
+        """ Constructor maping size to parent constructor"""
+        self.__size = size
+        self.integer_validator("size", self.__size)
+        super().__init__(self.__size, self.__size)
 
     def area(self):
-        """ method that return de area of rectangle """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ string representation """
-        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        """ Calling parent method area() """
+        return (super().area())
