d148c0f8647dab98ce12051c1cf90f20c5f7100f
root <r.irakoze@alustudent.com>
2023-03-23 04:57:49 -0700

    lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" this ispython script that
+will list of oobjects and available attributes
+"""
+
+
+def lookup(obj):
+
+    """look up function to deal with the problem
+    """
+
+    return dir(obj)
