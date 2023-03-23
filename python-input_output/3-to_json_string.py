875fec9a3c13e84c5166adc4c0fa52a830e9a25f
root <r.irakoze@alustudent.com>
2023-03-23 13:02:39 -0700

    return json

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
-""" Module that contains a function that reads n lines of a text file
-"""
+"""Defines a string-to-JSON function."""
+import json
 
 
-def read_lines(filename="", nb_lines=0):
-    """ Function that reads from a file and prints its number of lines
-    Args:
-        filename: filename
-        nb_lines: number of lines to print
-    Raises
-        Exception: when the file can be opened
-    """
-
-    with open(filename, 'r', encoding="utf-8") as f:
-        if nb_lines <= 0:
-            read_data = f.read()
-            print(read_data, end='')
-        else:
-            n_lines = 0
-            for line in f:
-                print(line, end='')
-                n_lines += 1
-                if n_lines == nb_lines:
-                    break
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
